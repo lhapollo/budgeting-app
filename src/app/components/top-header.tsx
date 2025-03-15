cfa4e707da6a17958543e1df2417c5d5d8bce4df
iris <irishu0909@gmail.com>
2025-03-15 18:10:23 -0400

    components!

---
+++ src/app/components/top-header.tsx
@@ -0,0 +1,9 @@
+const links = ["Home", "Profile"]
+export default function Header() {
+    return (
+        <nav className="bg-gray-800 flex justify-between">
+            {/* Logo */}
+            <ul></ul>
+        </nav>
+    );
+}
